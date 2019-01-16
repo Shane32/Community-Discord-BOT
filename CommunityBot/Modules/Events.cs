a2da6c9260d733992f753363012b4f2d9fe3f77d
Shane Krueger <shane@acdmail.com>
2019-01-16 17:39:47 -0500

    EvaluateResponse fix

--- CommunityBot/Modules/Events.cs
+++ CommunityBot/Modules/Events.cs
@@ -4,8 +4,10 @@ using CommunityBot.Helpers;
 using CommunityBot.Preconditions;
 using Discord;
 using Discord.Commands;
+using Discord.WebSocket;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -29,6 +31,13 @@ namespace CommunityBot.Modules
             //await Context.Channel.SendMessageAsync("Check your DMs.");
 
             var dmChannel = await Context.User.GetOrCreateDMChannelAsync();
+
+            await dmChannel.SendMessageAsync("Do you like this?");
+            var temp = (await dmChannel.AwaitMessage(msg => EvaluateResponse(msg, "yes", "no"))).Content;
+            await dmChannel.SendMessageAsync("Answer: " + temp);
+
+            bool EvaluateResponse(SocketMessage arg, params String[] options)
+                => options.Any(option => arg.Content.ToLower().Contains(option.ToLower()) && arg.Author.Id == Context.User.Id);
 
             await dmChannel.SendMessageAsync("Enter the event title:");
 
